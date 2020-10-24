02068d2f75b933ca947e448d0724703f64a36951
moai <moai.kim@kakao.com>
2020-10-24 10:49:34 +0900

    2839번 풀이 완료

--- 1924/main.py
+++ 1924/main.py
@@ -7,7 +7,7 @@ week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 f = open('data.txt', 'r')
 M, D = map(int, f.readline().split())
 day = D
-asfsafsafsa
+
 for m in range(1,13):
     if m==M:
         break
